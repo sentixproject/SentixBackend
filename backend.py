49d0addbc98d4fa5946af52cb11d297b86043e0c
sentixproject <157822176+sentixproject@users.noreply.github.com>
2024-02-17 22:13:24 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import pandas as pd
 from ntscraper import Nitter
 import requests
 
@@ -8,13 +7,15 @@ app = FastAPI()
 scraper = Nitter(0)
 API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment"
 headers = {"Authorization": "Bearer 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"}
-Working_URL="https://nitter.poast.org/"
-Check_URL='https://status.d420.de/api/v1/instances'
+
+Check_URL = 'https://status.d420.de/api/v1/instances'
+
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	print(response.json())
-	return response.json()[0]
+    response = requests.post(API_URL, headers=headers, json=payload)
+    print(response.json())
+    return response.json()[0]
+
 
 class ScraperRequest(BaseModel):
     text: str
@@ -22,40 +23,51 @@ class ScraperRequest(BaseModel):
     number: int
     language: str = "en"
 
+
 @app.get("/check")
 async def root():
-    return "Calling Sentix Application for every 10 min...."
+    return "Calling Sentix Application for every 10 min..."
+
 
 @app.post("/get_tweets")
 async def get_tweets(request: ScraperRequest):
+    
     try:
-	response = requests.get(Check_URL)
-	data=response.json()
-	work=[]
-	for i in data['hosts']:
-		if i['rss']:
-			work+=[i['url']]
-			Working_URL=i['url']
-	tweets=[]
-	i=0 
-	while (i<len(work)):
-		twt=scraper.get_tweets(request.text, mode=request.mode, number=request.number, language=request.language,instance=work[i])
-		if len(twt['tweets'])!=0:
-			Working_URL=work[i]
-			maintweet=twt
-			break
-		i+=1 
-        #tweets = scraper.get_tweets(request.text, mode=request.mode, number=request.number, language=request.language)
+        
+        response = requests.get(Check_URL)
+        data = response.json()
+        work = []
+        tweets = []
+        i = 0
+        Working_URL = None
+        for i in data['hosts']:
+            if i['rss']:
+                work += [i['url']]
+                Working_URL = i['url']
+        
+        print(Working_URL,"Stage 1")
+        print(work)
+        for url in work:
+            twt = scraper.get_tweets(request.text, mode=request.mode, number=request.number, language=request.language, instance=url)
+            print(twt)
+            if 'tweets' in twt and len(twt['tweets']) != 0:
+                Working_URL = url
+                tweets = twt
+                break
+        print(Working_URL,"Stage 2")
+        print(tweets)
+        # tweets = scraper.get_tweets(request.text, mode=request.mode, number=request.number, language=request.language)
         final_tweets = []
         for x in tweets['tweets']:
             if f"twitter.com/{request.text[1:]}" not in x['link']:
-                data = {'twitter_link': x['link'], 'text': x['text'],'score':query({"inputs":x['text']})}
+                data = {'twitter_link': x['link'], 'text': x['text'], 'score': query({"inputs": x['text']})}
                 final_tweets.append(data)
 
         return final_tweets
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during scraping: {str(e)}")
+
 
 if __name__ == "__main__":
     import uvicorn
